9842efdb26304016c1d07d968dfaa6f0937c5438
Ayoub BOUMZEBRA <boumzebraa@gmail.com>
2025-01-28 11:20:58 +0100

    Update AlbumRemoteDatasource.kt

--- app/src/main/java/com/example/spotifywithjetpack/data/dataSource/remote/AlbumRemoteDatasource.kt
+++ app/src/main/java/com/example/spotifywithjetpack/data/dataSource/remote/AlbumRemoteDatasource.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Headers
 
 
 interface AlbumRemoteDatasource {
-    @Headers("X-Rapidapi-Key: 0b20d6657fmsh1edde3ec5286468p1eaa58jsn9b6310496e18")
+    @Headers("X-Rapidapi-Key: ")
     @GET("albums/?ids=4Zoxsc06EUHRf5GrJPJZ54,2k8JXNLzRk04iNgEvxunK0,5TqSJDbSP3wZzEJyTyAs9i,5IcsHifVLGG0emBPlMZacj,2LsRvrM6gsxxpg1mN9Z2uA,0fDtkURAIQeQ7Kz7ZC60xi,1kTzTL0iOIE2IAGw83yY4O,56hrhzHAOsm5C7kSXzF4kh,5AgsHUKFxr5DApRCmulIqJ,7eQGtkzCgrIWDOe76E9F8t")
     suspend fun getAlbums() : AlbumModel
 }
